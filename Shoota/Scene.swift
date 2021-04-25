d5cbb856b0b9520c3b232ab300451c3d5593ba2d
Johnny Martinez <johnnymn@hotmail.com>
2021-04-24 23:32:11 -0700

    Implement the timing and placement of targets on the screen

--- Shoota/Scene.swift
+++ Shoota/Scene.swift
@@ -2,31 +2,75 @@ import SpriteKit
 import ARKit
 
 class Scene: SKScene {
+  // This label shows the player how
+  // many targets are currently visible.
+  let remainingLabel = SKLabelNode()
+
+  // Used to create new targets
+  // every 2 seconds.
+  var timer: Timer!
+
+  // How many targets have been created.
+  var targetsCreated = 0
+
+  // How many targets are currently visible.
+  var targetCount = 0 {
+    // Update the remainingLabel automatically
+    // after this property is set to a new value.
+    didSet {
+      remainingLabel.text = "Remaining: \(targetCount)"
+    }
+  }
 
   override func didMove(to view: SKView) {
-    // Setup your scene here
+    remainingLabel.fontSize = 36
+    remainingLabel.fontName = "AmericanTypewriter"
+    remainingLabel.color = .white
+    // The remainingLabel needs to be positioned
+    // statically on the screen because its part
+    // of the HUD.
+    remainingLabel.position = CGPoint(x: frame.midX, y: frame.midY + 120)
+    addChild(remainingLabel)
+    targetCount = 0
+
+    timer = Timer.scheduledTimer(withTimeInterval: 2, repeats: true) {
+      timer in
+      self.createTarget()
+    }
   }
 
-  override func update(_ currentTime: TimeInterval) {
-    // Called before each frame is rendered
-  }
+  /// Creates new targets and places them on the screen.
+  func createTarget() {
+    // End the game if we created
+    // all the targets already.
+    if targetsCreated == 20 {
+      timer?.invalidate()
+      timer = nil
+      return
+    }
 
-  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    targetsCreated += 1
+    targetCount += 1
+
+    // Find the scene view.
     guard let sceneView = view as? ARSKView else {
       return
     }
 
-    // Create anchor using the camera's current position
-    if let currentFrame = sceneView.session.currentFrame {
+    // Create random x rotation.
+    let xRotation = simd_float4x4(SCNMatrix4MakeRotation(Float.pi * 2 * Float.random(in: 0...1), 1, 0, 0))
+    // Create random y rotation.
+    let yRotation = simd_float4x4(SCNMatrix4MakeRotation(Float.pi * 2 * Float.random(in: 0...1), 0, 1, 0))
+    // Combine them.
+    let rotation = simd_mul(xRotation, yRotation)
+    // Move forward 1.5m into the screen.
+    var translation = matrix_identity_float4x4
+    translation.columns.3.z = -1.5
+    // Combine the translation and rotation.
+    let transform = simd_mul(rotation, translation)
 
-      // Create a transform with a translation of 0.2 meters in front of the camera
-      var translation = matrix_identity_float4x4
-      translation.columns.3.z = -0.2
-      let transform = simd_mul(currentFrame.camera.transform, translation)
-
-      // Add a new anchor to the session
-      let anchor = ARAnchor(transform: transform)
-      sceneView.session.add(anchor: anchor)
-    }
+    // Create an anchor at the calculated position.
+    let anchor = ARAnchor(transform: transform)
+    sceneView.session.add(anchor: anchor)
   }
 }
--- Shoota/ViewController.swift
+++ Shoota/ViewController.swift
@@ -26,7 +26,7 @@ class ViewController: UIViewController, ARSKViewDelegate {
     super.viewWillAppear(animated)
 
     // Create a session configuration
-    let configuration = ARWorldTrackingConfiguration()
+    let configuration = AROrientationTrackingConfiguration()
 
     // Run the view's session
     sceneView.session.run(configuration)
@@ -39,25 +39,7 @@ class ViewController: UIViewController, ARSKViewDelegate {
     sceneView.session.pause()
   }
 
-  // MARK: - ARSKViewDelegate
-
   func view(_ view: ARSKView, nodeFor anchor: ARAnchor) -> SKNode? {
-    // Create and configure a node for the anchor added to the view's session.
-    let labelNode = SKLabelNode(text: "👾")
-    labelNode.horizontalAlignmentMode = .center
-    labelNode.verticalAlignmentMode = .center
-    return labelNode;
-  }
-
-  func session(_ session: ARSession, didFailWithError error: Error) {
-    // Present an error message to the user
-  }
-
-  func sessionWasInterrupted(_ session: ARSession) {
-    // Inform the user that the session has been interrupted, for example, by presenting an overlay
-  }
-
-  func sessionInterruptionEnded(_ session: ARSession) {
-    // Reset tracking and/or remove existing anchors if consistent tracking is required
+    return SKSpriteNode(imageNamed: "target")
   }
 }
